a62d45af0b2eef0379bf8cb3f6d4ed6b0cea44fe
ethanresek <resekethan@gmail.com>
2026-04-25 16:23:12 -0400

    Update getEvents

--- server/controllers/events.js
+++ server/controllers/events.js
@@ -1,9 +1,76 @@
 import { pool } from "../config/database.js"
 
 const getEvents = async (req, res) => {
+
   try {
-    const results = await pool.query("SELECT * FROM events WHERE user_id = $1 ORDER BY date;", [req.user.uid]);
+    const query = `
+      SELECT e.name, et.name AS event_type, e.description, e.date, e.budget, e.recurring
+      FROM events AS e
+      LEFT JOIN event_types AS et
+        ON e.type_id = et.id
+      WHERE user_id = $1
+      ORDER BY e.date;`;
+
+    const results = await pool.query(query, [req.user.uid]);
+
     res.status(200).json(results.rows);
+  } catch (error) {
+    console.error({ message: error });
+    res.status(500).json({ error: error.message });
+  }
+}
+
+const getEventById = async (req, res) => {
+  try {
+    const query = `
+    WITH gift_agg AS (
+      SELECT contact_id, json_agg(
+        json_build_object(
+          'id', id,
+          'name', name,
+          'price', price,
+          'status', status,
+          'description', description,
+          'url', url
+        )
+      ) AS gifts
+      FROM gift_ideas
+      GROUP BY contact_id
+    )
+    SELECT e.name, e.description, e.date, e.budget, e.recurring, et.name AS event_type,
+      json_agg(
+        json_build_object(
+          'id', c.id,
+          'name', c.name,
+          'relationship', c.relationship,
+          'email', c.email,
+          'phone_number', c.phone_number,
+          'notes', c.notes,
+          'gifts', ga.gifts
+        )
+      )
+    FROM events AS e
+    JOIN event_types AS et
+    ON e.type_id = et.id
+    JOIN event_contacts AS ec
+    ON e.id = event_id
+    JOIN contacts AS c
+    ON ec.contact_id = c.id
+    LEFT JOIN gift_agg ga
+    ON c.id = ga.contact_id
+    WHERE e.id = $1 AND e.user_id = $2
+    GROUP BY e.id, e.name, e.description, e.date, e.budget, e.recurring, et.name
+    ;`
+
+    const event_id = parseInt(req.params.id);
+    const user_id = req.user.uid;
+
+    const results = await pool.query(query, [event_id, user_id]);
+    if (results.rows.length === 0) {
+      return res.status(404).json({ error: 'Event not found' });
+    }
+    res.status(200).json(results.rows[0]);
+
   } catch (error) {
     console.error({ message: error });
     res.status(500).json({ error: error.message });
@@ -13,14 +80,14 @@ const getEvents = async (req, res) => {
 const createEvent = async (req, res) => {
 
   try {
-    const { user_id, name, type_id, description, date, budget, recurring } = req.body;
+    const { name, type_id, description, date, budget, recurring } = req.body;
 
     const query = `
     INSERT INTO events (user_id, name, type_id, description, date, budget, recurring)
     VALUES ($1, $2, $3, $4, $5, $6, $7)
     RETURNING *;`
 
-    const results = await pool.query(query, [user_id, name, type_id, description, date, budget, recurring])
+    const results = await pool.query(query, [req.user.uid, name, type_id, description, date, budget, recurring])
     res.status(201).json(results.rows[0]);
   } catch (error) {
     res.status(500).json({ error: error.message })
@@ -65,6 +132,7 @@ const deleteEvent = async (req, res) => {
 
 export default {
   getEvents,
+  getEventById,
   createEvent,
   updateEvent,
   deleteEvent

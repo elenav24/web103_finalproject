95a542ea07f377128337d31986ae9b0f2f9596a8
ethanresek <resekethan@gmail.com>
2026-04-25 16:23:24 -0400

    Update server paths

--- server/server.js
+++ server/server.js
@@ -4,15 +4,19 @@ import './config/dotenv.js'
 import cors from 'cors'
 import eventsRouter from './routes/events.js'
 import contactsRouter from './routes/contacts.js'
-
+import usersRouter from './routes/users.js'
+import giftsRouter from './routes/gifts.js'
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
 app.use('/api/events', eventsRouter);
 app.use('/api/contacts', contactsRouter);
+app.use('/api/contacts', usersRouter);
+app.use('/api/gifts', giftsRouter);
 
 app.get('/', (req, res) => {
   res.status(200).send('<h1 style="text-align: center; margin-top: 50px;">GiftGiver API');
